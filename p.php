ce0865f4e570eda18fbbacfb02dbaf7b09b8cf24
Wh1t3 shadow <47253920+wh1t3-shadow@users.noreply.github.com>
2020-11-19 19:11:55 +0600

    Add files via upload

---
+++ p.php
@@ -0,0 +1,67 @@
+
+<?php
+
+system("clear");
+echo"
+
+
+";
+
+echo"\033[1;32m ------‐------------------------------------------";
+
+echo"
+";
+echo"\033[1;36m               Author : wh1t3 shadow    ";
+     
+echo"
+";
+echo "\033[1;36m              Bangladesh Cyber Ghost      ";
+      
+echo"
+";
+echo"\033[1;32m -----‐-----------‐--------------------------------";
+echo"
+
+
+";
+$a= readline("your code name : ");
+$d= readline("\033[1;35m your title : ");
+$t= readline("\033[1;33m your team name : ");
+              
+
+echo"
+
+";
+
+echo"            copy this html code         ";
+echo"
+
+";
+echo " ﻿\033[0;36m
+
+﻿<html>
+ <head>
+ <title>hacked</title>
+ </head> 
+<body bgcolor=black>
+ <br> <br> <br> <br> 
+<center>
+ <marquee> <font color= #00FF00 size=8> $a</font> </marquee>
+ <hr color=blue width=90%>
+ <br> <br>
+ <center><p style='color:red; font-size:45px;'>$d</h1></center> 
+<br> <br> <br> 
+<img src='https://i.gifer.com/SxeZ.gif' height='700px' width='750px'>
+ </center>
+<br> <br> <br> 
+<center>
+ <font size=7 color=blue>Don't play with me <hr color=blue width=50%> <hr color=blue width=50%>$t  <hr color=blue width=50%>
+</font> 
+</center>
+</body>
+ </html> 
+
+
+";
+
+?>
