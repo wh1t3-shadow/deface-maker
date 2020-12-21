910d9c9266b1852d1040e2c1e1df91d2ddde2e45
Wh1t3 shadow <47253920+wh1t3-shadow@users.noreply.github.com>
2020-12-21 22:09:15 +0600

    Update p.php

--- p.php
+++ p.php
@@ -1,4 +1,3 @@
-
 <?php
 
 system("clear");
@@ -12,11 +11,11 @@ echo"\033[1;32m ------‐------------------------------------------";
 echo"
 ";
 echo"\033[1;36m               Author : wh1t3 shadow    ";
-     
+
 echo"
 ";
 echo "\033[1;36m              Bangladesh Cyber Ghost      ";
-      
+
 echo"
 ";
 echo"\033[1;32m -----‐-----------‐--------------------------------";
@@ -27,41 +26,99 @@ echo"
 $a= readline("your code name : ");
 $d= readline("\033[1;35m your title : ");
 $t= readline("\033[1;33m your team name : ");
-              
+
 
 echo"
 
 ";
 
-echo"            copy this html code         ";
+
 echo"
 
 ";
+
+/*
 echo " ﻿\033[0;36m
 
 ﻿<html>
  <head>
  <title>hacked</title>
- </head> 
+ </head>
 <body bgcolor=black>
- <br> <br> <br> <br> 
+ <br> <br> <br> <br>
 <center>
  <marquee> <font color= #00FF00 size=8> $a</font> </marquee>
  <hr color=blue width=90%>
  <br> <br>
- <center><p style='color:red; font-size:45px;'>$d</h1></center> 
-<br> <br> <br> 
+ <center><p style='color:red; font-size:45px;'>$d</h1></center>
+<br> <br> <br>
 <img src='https://i.gifer.com/CX6Z.gif' height='700px' width='750px'>
  </center>
-<br> <br> <br> 
+<br> <br> <br>
 <center>
  <font size=7 color=blue>Don't play with me <hr color=blue width=50%> <hr color=blue width=50%>$t  <hr color=blue width=50%>
-</font> 
+</font>
 </center>
 </body>
- </html> 
+ </html>
 
 
 ";
+*/
 
+
+
+
+
+$myfile = fopen("deface.html", "w") or die("Unable to open file!");
+
+
+$txt = "
+﻿<html>
+ <head>
+ <title>hacked</title>
+ </head>
+<body bgcolor=black>
+ <br> <br> <br> <br>
+<center>
+ <marquee> <font color= #00FF00 size=8> $a</font> </marquee>
+ <hr color=blue width=90%>
+ <br> <br>
+ <center><p style='color:red; font-size:45px;'>$d</h1></center>
+<br> <br> <br>
+<img src='https://i.gifer.com/CX6Z.gif' height='700px' width='750px'>
+ </center>
+<br> <br> <br>
+<center>
+ <font size=7 color=blue>Don't play with me <hr color=blue width=50%> <hr color=blue width=50%>$t  <hr color=blue width=50%>
+</font>
+</center>
+</body>
+ </html>
+";
+
+
+fwrite($myfile, $txt);
+fclose($myfile);
+
+
+system("clear");
+system("cp deface.html /sdcard");
+echo"
+
+
+";
+echo"\033[1;35m       This deface page saved to your phone storage";
+
+
+
+echo"
+
+";
+
+echo"                 Thanks for useing this tool";
+
+echo"
+
+";
 ?>
